1eaa73f282e018fb3a4738fcd16c6474a695392a
claire <clairebontempo@gmail.com>
2021-01-27 18:14:51 -0800

    commit tree 1c9f6b2daac7b8efa1749e8337bb1ad143cd00e5

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(input) {
+  const time = parseInt(input.split(':'));
+  if (time < 12) {
+   return "Good Morning";
+  } else if (time >= 12 && time <= 17) {
+    return "Good Afternoon";
+  } else {
+  return "Good Evening";
+  }
+}
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg
+}
+
 /* Write your implementation of displayMessage() */
+
+
